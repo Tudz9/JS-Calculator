f9478fa5bfa5b52f3521b8360fcca6c700ce0577
Lydia <166108995+Tudz9@users.noreply.github.com>
2025-06-04 11:45:16 +0200

    lesson 2: Event Handlers

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ function appendToDisplay(value) {
     
     alert('You pressed' + value);
 }
+
+function clearDisplay() {
+    console.log('Clear button pressed.');
+    alert('Clear button was clicked');
+}
+
+function deleteLast() {
+    console.log('Backspace button pressed.');
+    alert('Backspace button was clicked');
+}
+
+function calculate() {
+    console.log('Equals button pressed.');
+    alert('Equals button was clicked');
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Calculator loaded successfully');
+    console.log('Display elemt', display);
+    
+    if (display) {
+        console.log('Current display value:', display.value);
+    } else{
+        console.log('Display element not found');
+    }
+})
